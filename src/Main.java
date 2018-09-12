8f743b2b2c8052688876b5a4ae3e0ec23212dc38
Kálmán Zsolt <zsolter14@gmail.com>
2018-09-12 12:15:50 +0200

    my newest feature

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
 
     public static void main(String[] args) {
-        
+        JPanel myPanel = new JPanel(new GridBagLayout());
+        JLabel text = new JLabel("Szia!");
+        myPanel.add(text);
+
+
     }
+
 }
